098ad76a57ff0065ad9fc724491cab8c5e51ecfc
Lokesh b s <84077131+lokesbs0608@users.noreply.github.com>
2025-06-04 01:25:08 +0530

    styles separated

--- src/components/loader/index.js
+++ src/components/loader/index.js
@@ -1,45 +1,10 @@
 import React from "react";
+import styles from "./styles.module.css";
 
-const Loader = () => {
-    return (
-        <div style={styles.overlay}>
-            <div style={styles.loader}></div>
-        </div>
-    );
-};
-
-const styles = {
-    overlay: {
-        position: "fixed",
-        top: 0,
-        left: 0,
-        width: "100vw",
-        height: "100vh",
-        backgroundColor: "rgba(0,0,0,0.5)", // semi-transparent dark overlay
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        zIndex: 9999, // high z-index to cover everything
-        pointerEvents: "auto", // catch pointer events to prevent clicks
-    },
-    loader: {
-        border: "8px solid #f3f3f3", // Light grey
-        borderTop: "8px solid #3498db", // Blue
-        borderRadius: "50%",
-        width: "60px",
-        height: "60px",
-        animation: "spin 1s linear infinite",
-    },
-};
-
-// Add keyframe animation to document
-const styleSheet = document.styleSheets[0];
-const keyframes =
-    `@keyframes spin {
-    0% { transform: rotate(0deg); }
-    100% { transform: rotate(360deg); }
-  }`;
-
-styleSheet.insertRule(keyframes, styleSheet.cssRules.length);
+const Loader = () => (
+    <div className={styles.overlay}>
+        <div className={styles.loader}></div>
+    </div>
+);
 
 export default Loader;
